2d039e3b432d9f661937f4e9d22d06dc1a10179e
maze-n <mmaz999@outlook.com>
2020-09-21 15:16:54 +0530

    Add default value to mobile in user model

--- server/models/user.js
+++ server/models/user.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  mobile: { type: String, required: false, minlength: 10, maxlength: 10 },
+  mobile: { type: String, required: false, default: '' },
   password: { type: String, required: true, minlength: 8 }
 })
 
