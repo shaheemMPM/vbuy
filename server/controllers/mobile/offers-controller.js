56e6c024a48d452b658841fbae019d64bea185c6
Mazen Maliyakkal <mmaz999@outlook.com>
2020-10-01 22:17:47 +0530

    Change getOffers method to getOffersByShopId

--- server/controllers/mobile/offers-controller.js
+++ server/controllers/mobile/offers-controller.js
@@ -4,11 +4,12 @@ const HttpError = require('../../models/http-error');
 const Offers = require('../../models/offers');
 const Products = require('../../models/products');
 
-const getOffers = async (req, res, next) => {
+const getOffersByShopId = async (req, res, next) => {
+	let shopId = req.params.sid;
 	let offers;
 
 	try {
-		offers = await Offers.find().select('name image percentage');
+		offers = await Offers.find({shopId: shopId}).select('name image percentage');
 	} catch (error) {
 		return next(new HttpError('Reading offers failed.', 500));
 	}
@@ -29,5 +30,5 @@ const getProducts = async (req, res, next) => {
 	res.status(200).json({products});
 }
 
-exports.getOffers = getOffers;
+exports.getOffersByShopId = getOffersByShopId;
 exports.getProducts = getProducts;
