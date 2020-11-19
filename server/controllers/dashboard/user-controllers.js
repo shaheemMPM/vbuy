244eb08f825854e574766cdf18d7db4adb9957a2
shaheemMPM <shaheem.mpm@gmail.com>
2020-11-19 10:36:55 +0530

    Get orders of given user

--- server/controllers/dashboard/user-controllers.js
+++ server/controllers/dashboard/user-controllers.js
@@ -1,5 +1,6 @@
 const HttpError = require('../../models/http-error');
 const Users = require('../../models/user');
+const Orders = require('../../models/orders');
 
 const getUsers = async (req, res, next) => {
   let users;
@@ -14,19 +15,34 @@ const getUsers = async (req, res, next) => {
 const getUserById = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let user;
+  let user, orders;
 
   try {
     user = await Users.findById(userId).select('-password -__v');
   } catch (error) {
-    return next(new HttpError('Something went wrong, could not find a shop with given id.', 500));
+    return next(new HttpError('Something went wrong, could not find a user with given id.', 500));
+  }
+
+  try {
+    orders = await Orders.find({userId: userId});
+  } catch (error) {
+    return next(new HttpError('Something went wrong, could not find a orders for given user', 500));
   }
 
   if (!user) {
     return next(new HttpError('Could not find a user for the provided id.', 404));
   }
 
-  res.json({ user });
+  res.json({ 
+            user: {
+                    name: user.name, 
+                    _id: user._id, 
+                    mobile: user.mobile, 
+                    email: user.email, 
+                    orders: orders,
+                    timestamp: user._id.getTimestamp()
+                  } 
+            });
 };
 
 exports.getUsers = getUsers;
